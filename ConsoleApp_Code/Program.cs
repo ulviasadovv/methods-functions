7de8f2750b42dec53b6056764b697d1725413d73
ulviasadovv <ulvisha@code.edu.az>
2023-10-31 14:40:17 +0400

    first commit

---
+++ ConsoleApp_Code/Program.cs
@@ -0,0 +1,62 @@
+﻿//Task 1
+int sum(int num1, int num2)
+{
+    int result = num1 + num2;
+    return result;
+};
+
+Console.WriteLine(sum(2, 5));
+
+
+
+//Task 2
+static int CircleArea(int r)
+{
+    int result = 3 * (r * r);
+    return result;
+};
+
+
+static int RectangleArea(int a, int b)
+{
+    int result = a * b;
+    return result;
+};
+
+
+static int CirInTriArea(int a, int b, int c, int r)
+{
+    int p = (a + b + c) / 2;
+    int result = p * r;
+    return result;
+};
+
+Console.WriteLine(CircleArea(5));
+Console.WriteLine(RectangleArea(3, 8));
+Console.WriteLine(CirInTriArea(1, 2, 5, 3));
+
+
+
+//Task 3
+int allSum(int a)
+{
+    int result = 0;
+    int sum = 0;
+    while (sum < a)
+    {
+        result++;
+
+
+        if ((sum += result) <= a)
+        {
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    };
+    return sum;
+};
+
+Console.WriteLine(allSum(11));
